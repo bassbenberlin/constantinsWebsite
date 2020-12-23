1fdc4692cb92cb155e5363ad3ec2f3b5d177f3da
Benjamin Kühne <egbert.benjamin.kuehne@gmail.com>
2020-12-23 14:32:56 +0100

    Version vom 23.12.2020

---
+++ js/appStart.js
@@ -0,0 +1,7 @@
+$(document).ready(function() 
+{
+   // alles was hierdrin steht, wird aufgerufen nachdem die website auf das 
+   // gerät geladen wurde vom user (dem der die website anschauen will)
+   
+   $('#drumrum').show(600);
+});
